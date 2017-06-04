6c90c70523fc203bd370e01ab804a536ba0395d2
philip-r-butler <philip.r.butler@gmail.com>
2017-06-04 18:40:42 +0100

    Added unit tests for check order, getting single order by index, getting count of orders with value.

--- tests/unit/checkout-order.test.js
+++ tests/unit/checkout-order.test.js
@@ -24,7 +24,7 @@ describe('Tests for checkout-orders module', function () {
         order.add(key);
 
         expect(order.count()).toBe(1);
-    })
+    });
 
     it('Add multiple orders', function () {
         var key;
@@ -35,6 +35,24 @@ describe('Tests for checkout-orders module', function () {
         order.add(key);
 
         expect(order.count()).toBe(2);
+    });
+
+    it('Get order item', function () {
+        var key1, key2, key3;
+
+        key1 = 'A';
+        key2 = 'B';
+        key3 = 'C';
+
+        order.add(key1);
+        order.add(key2);
+        order.add(key1);
+        order.add(key1);
+        order.add(key3);
+
+        expect(order.getItem(3)).toBe(key1);
+        expect(order.getItem(1)).toBe(key2);
+        expect(order.getItem(4)).toBe(key3);
     })
 
     it('Remove last order', function () {
@@ -47,7 +65,7 @@ describe('Tests for checkout-orders module', function () {
         order.removeLast();
 
         expect(order.count()).toBe(1);
-    })
+    });
 
     it('Get all orders', function () {
         var key1, key2, ordered;
@@ -62,5 +80,26 @@ describe('Tests for checkout-orders module', function () {
 
         expect(ordered[0]).toBe(key1);
         expect(ordered[1]).toBe(key2);
-    })
+    });
+
+    it('Count orders with key', function () {
+        var key1, key2, key3;
+
+        key1 = 'A';
+        key2 = 'B';
+        key3 = 'C';
+
+        order.add(key1);
+        order.add(key2);
+        order.add(key1);
+        order.add(key1);
+        order.add(key2);
+        order.add(key3);
+        order.add(key1);
+
+        expect(order.countItem(key1)).toBe(4);
+        expect(order.countItem(key2)).toBe(2);
+        expect(order.countItem(key3)).toBe(1);
+
+    });
 });
