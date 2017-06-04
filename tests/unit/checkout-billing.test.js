de7ee622f5bad2bcdd0b5c1bfd2bfc509dca5754
philip-r-butler <philip.r.butler@gmail.com>
2017-06-04 23:16:46 +0100

    Added more completed tests with combinations of stock keeping items with and without discounts

--- tests/unit/checkout-billing.test.js
+++ tests/unit/checkout-billing.test.js
@@ -61,4 +61,221 @@ describe('Tests for checkout-billing module', function () {
 
         expect(bill.get()).toBe(195);
     });
+
+    it('Calculate bill with single item type in order, with discount but discount limit not reached', function () {
+        var key1;
+
+        key1 = 'A';
+
+        order.add(key1);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 45;
+                    },
+                    limit: 3
+                }
+            }
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(100);
+
+    });
+
+    it('Calculate bill with single item type in order, with discount and discount limit reached', function () {
+        var key1;
+
+        key1 = 'A';
+
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 130 / 3;
+                    },
+                    limit: 3
+                }
+            }
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(130);
+
+    });
+
+    it('Calculate bill with single item type in order, with discount and discount limit exceeded', function () {
+        var key1;
+
+        key1 = 'A';
+
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 130 / 3;
+                    },
+                    limit: 3
+                }
+            }
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(230);
+
+    });
+
+    it('Calculate bill with single item type in order, with discount and discount limit exceeded twice', function () {
+        var key1;
+
+        key1 = 'A';
+
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 130 / 3;
+                    },
+                    limit: 3
+                }
+            }
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(260);
+
+    });
+
+    it('Calculate bill with two item types in order, with and without discount and discount limit exceeded', function () {
+        var key1, key2;
+
+        key1 = 'A';
+        key2 = 'B';
+
+        order.add(key1);
+        order.add(key2);
+        order.add(key1);
+        order.add(key1);
+        order.add(key2);
+        order.add(key2);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 130 / 3;
+                    },
+                    limit: 3
+                }
+            },
+            'B': {
+                label: 'Label for B',
+                price: 30,
+                discount: {
+                    price: function () {
+                        return 45 / 2;
+                    },
+                    limit: 2
+                }
+            }
+
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(255);
+
+    });
+
+    it('Calculate bill with four item types in order, with and without discount and discount limit exceeded', function () {
+        var key1, key2, key3, key4;
+
+        key1 = 'A';
+        key2 = 'B';
+        key3 = 'C';
+        key4 = 'D';
+
+        order.add(key1);
+        order.add(key2);
+        order.add(key1);
+        order.add(key1);
+        order.add(key2);
+        order.add(key2);
+        order.add(key1);
+        order.add(key4);
+        order.add(key3);
+        order.add(key4);
+        order.add(key1);
+
+        sku.set({
+            'A': {
+                label: 'Label for A',
+                price: 50,
+                discount: {
+                    price: function () {
+                        return 130 / 3;
+                    },
+                    limit: 3
+                }
+            },
+            'B': {
+                label: 'Label for B',
+                price: 30,
+                discount: {
+                    price: function () {
+                        return 45 / 2;
+                    },
+                    limit: 2
+                }
+            },
+            'C': {
+                label: 'Label for C',
+                price: 20
+            },
+            'D': {
+                label: 'Label for C',
+                price: 15
+            }
+        });
+
+        bill.update();
+
+        expect(bill.get()).toBe(355);
+
+    });
 });
