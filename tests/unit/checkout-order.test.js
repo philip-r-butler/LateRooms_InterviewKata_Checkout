a3ac4c94c4b4514ae8d48d7679fdab2a86d5a68c
philip-r-butler <philip.r.butler@gmail.com>
2017-06-14 20:40:38 +0100

    Updating tests to use chaining methods.

--- tests/unit/checkout-order.test.js
+++ tests/unit/checkout-order.test.js
@@ -2,9 +2,9 @@
  * Created by Phil Butler on 04/06/2017.
  */
 /*global lateRooms */
-describe('Tests for checkout-orders module', function () {
+describe("Tests for checkout-orders module", function () {
 
-    'use strict';
+    "use strict";
 
     var order;
 
@@ -16,62 +16,53 @@ describe('Tests for checkout-orders module', function () {
         order.clear();
     });
 
-    it('Add single order', function () {
+    it("Add single order", function () {
         var key;
 
-        key = 'A';
+        key = "A";
 
         order.add(key);
 
         expect(order.count()).toBe(1);
     });
 
-    it('Add multiple orders', function () {
-        var key;
+    it("Add multiple orders", function () {
 
-        key = 'A';
-        order.add(key);
-        key = 'B';
-        order.add(key);
+        order.add("A").add("B");
 
         expect(order.count()).toBe(2);
     });
 
-    it('Get order item', function () {
-        var key1, key2, key3;
+    it("Get order item", function () {
+        var key1;
+        var key2;
+        var key3;
 
-        key1 = 'A';
-        key2 = 'B';
-        key3 = 'C';
+        key1 = "A";
+        key2 = "B";
+        key3 = "C";
 
-        order.add(key1);
-        order.add(key2);
-        order.add(key1);
-        order.add(key1);
-        order.add(key3);
+        order.add(key1).add(key2).add(key1).add(key1).add(key3);
 
         expect(order.getItem(3)).toBe(key1);
         expect(order.getItem(1)).toBe(key2);
         expect(order.getItem(4)).toBe(key3);
     });
 
-    it('Remove last order', function () {
-        var key;
+    it("Remove last order", function () {
 
-        key = 'A';
-        order.add(key);
-        key = 'B';
-        order.add(key);
-        order.removeLast();
+        order.add("A").add("B").removeLast();
 
         expect(order.count()).toBe(1);
     });
 
-    it('Get all orders', function () {
-        var key1, key2, ordered;
+    it("Get all orders", function () {
+        var key1;
+        var key2;
+        var ordered;
 
-        key1 = 'A';
-        key2 = 'B';
+        key1 = "A";
+        key2 = "B";
 
         order.add(key1);
         order.add(key2);
@@ -82,20 +73,16 @@ describe('Tests for checkout-orders module', function () {
         expect(ordered[1]).toBe(key2);
     });
 
-    it('Count orders with key', function () {
-        var key1, key2, key3;
+    it("Count orders with key", function () {
+        var key1;
+        var key2;
+        var key3;
 
-        key1 = 'A';
-        key2 = 'B';
-        key3 = 'C';
+        key1 = "A";
+        key2 = "B";
+        key3 = "C";
 
-        order.add(key1);
-        order.add(key2);
-        order.add(key1);
-        order.add(key1);
-        order.add(key2);
-        order.add(key3);
-        order.add(key1);
+        order.add(key1).add(key2).add(key1).add(key1).add(key2).add(key3).add(key1);
 
         expect(order.countItem(key1)).toBe(4);
         expect(order.countItem(key2)).toBe(2);
