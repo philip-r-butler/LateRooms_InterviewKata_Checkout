2a98e0888bec448782c96058667d40b526c3a8b3
Philip butler <philip.butler@ticketmaster.co.uk>
2017-07-06 11:59:44 +0100

    Updated to work on Karma and Mocha, uses chai.expect

--- tests/unit/checkout-order.spec.js
+++ tests/unit/checkout-order.spec.js
@@ -1,24 +1,29 @@
 /**
  * Created by Phil Butler on 04/06/2017.
  */
-/*global lateRooms */
+/*global lateRooms, chai */
 describe("Tests for checkout-orders module", function () {
 
     "use strict";
 
-    var order;
-    var sku;
     var rules;
+    var order;
+    var expect;
 
     beforeEach(function () {
-        order = lateRooms.kata.checkout.order;
-        sku = lateRooms.kata.checkout.stockKeepingUnits;
-        rules = lateRooms.kata.checkout.rules;
+        if (typeof(module) === "undefined") {
+            expect = chai.expect;
+            rules = lateRooms.kata.checkout.chargeRules;
+            order = lateRooms.kata.checkout.order;
+        } else {
+            expect = require("chai").expect;
+            rules = require("../../js/checkout-chargerules");
+            order = require("../../js/checkout-order");
+        }
     });
 
     afterEach(function () {
         order.clear();
-        sku.clear();
     });
 
     it("Add single order", function () {
@@ -28,14 +33,14 @@ describe("Tests for checkout-orders module", function () {
 
         order.add(key);
 
-        expect(order.count()).toBe(1);
+        expect(order.count()).to.equal(1);
     });
 
     it("Add multiple orders", function () {
 
         order.add("A").add("B");
 
-        expect(order.count()).toBe(2);
+        expect(order.count()).to.equal(2);
     });
 
     it("Get order item", function () {
@@ -49,16 +54,16 @@ describe("Tests for checkout-orders module", function () {
 
         order.add(key1).add(key2).add(key1).add(key1).add(key3);
 
-        expect(order.getItem(3)).toBe(key1);
-        expect(order.getItem(1)).toBe(key2);
-        expect(order.getItem(4)).toBe(key3);
+        expect(order.getItem(3)).to.equal(key1);
+        expect(order.getItem(1)).to.equal(key2);
+        expect(order.getItem(4)).to.equal(key3);
     });
 
     it("Remove last order", function () {
 
         order.add("A").add("B").removeLast();
 
-        expect(order.count()).toBe(1);
+        expect(order.count()).to.equal(1);
     });
 
     it("Get all orders", function () {
@@ -74,8 +79,8 @@ describe("Tests for checkout-orders module", function () {
 
         ordered = order.get();
 
-        expect(ordered[0]).toBe(key1);
-        expect(ordered[1]).toBe(key2);
+        expect(ordered[0]).to.equal(key1);
+        expect(ordered[1]).to.equal(key2);
     });
 
     it("Count orders with key", function () {
@@ -89,21 +94,21 @@ describe("Tests for checkout-orders module", function () {
 
         order.add(key1).add(key2).add(key1).add(key1).add(key2).add(key3).add(key1);
 
-        expect(order.countItem(key1)).toBe(4);
-        expect(order.countItem(key2)).toBe(2);
-        expect(order.countItem(key3)).toBe(1);
+        expect(order.countItem(key1)).to.equal(4);
+        expect(order.countItem(key2)).to.equal(2);
+        expect(order.countItem(key3)).to.equal(1);
 
     });
 
     it("Get order charge with multiple order items of same stock keeping unit", function () {
         var key1;
+        var units;
 
         key1 = "A";
 
-        order.add(key1);
-        order.add(key1);
+        order.add(key1).add(key1);
 
-        sku.set({
+        units = {
             "A": {
                 label: "Label for A",
                 rule: {
@@ -113,26 +118,20 @@ describe("Tests for checkout-orders module", function () {
                     }
                 }
             }
-        });
+        };
 
-        expect(order.charge()).toBe(100);
+        expect(order.setUnits(units).charge()).to.equal(100);
     });
 
     it("Get order charge with multiple order items of different stock keeping unit", function () {
         var key1;
         var key2;
+        var units;
 
         key1 = "A";
         key2 = "B";
 
-        order.add(key2);
-        order.add(key1);
-        order.add(key2);
-        order.add(key1);
-        order.add(key1);
-        order.add(key1);
-
-        sku.set({
+        units = {
             "A": {
                 label: "Label for A",
                 rule: {
@@ -151,8 +150,10 @@ describe("Tests for checkout-orders module", function () {
                     }
                 }
             }
-        });
+        };
 
-        expect(order.charge()).toBe(400);
+        order.add(key2).add(key1).add(key2).add(key1).add(key1).add(key1);
+
+        expect(order.setUnits(units).charge()).to.equal(400);
     });
 });
