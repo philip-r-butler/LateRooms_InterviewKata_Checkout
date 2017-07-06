fc41d902e3c3ccd7800c3b35542b2d644882e87e
Philip butler <philip.butler@ticketmaster.co.uk>
2017-07-06 11:57:20 +0100

    Refactored to run in node and browser

--- js/checkout-chargerules.js
+++ js/checkout-chargerules.js
@@ -2,11 +2,12 @@
  * Created by mtlsspb4 on 14/06/2017.
  */
 /*global lateRooms*/
-(function (checkout) {
+(function () {
     "use strict";
 
-    checkout.rules = (function () {
+    var chargeRules;
 
+    chargeRules = (function () {
         return {
             fixedPrice: function (obj) {
                 return obj.price * obj.count;
@@ -30,4 +31,44 @@
         };
     }());
 
-}(lateRooms.kata.checkout));
+    if (typeof module === "undefined") {
+        lateRooms.kata.checkout.chargeRules = chargeRules;
+    } else {
+        module.exports = chargeRules;
+    }
+
+}());
+
+// (function () {
+//     "use strict";
+//
+//     var rules;
+//
+//     rules = (function () {
+//
+//         return {
+//             fixedPrice: function (obj) {
+//                 return obj.price * obj.count;
+//             },
+//             discountPriceWithOrderLimit: function (obj) {
+//                 var countAtDiscountPrice;
+//
+//                 countAtDiscountPrice = (function (count, limit) {
+//                     if (limit && count) {
+//                         return Math.floor(count / limit) * limit;
+//                     } else {
+//                         return null;
+//                     }
+//                 }(obj.count, obj.discountLimit));
+//
+//                 return (obj.count * obj.fullPrice) + (countAtDiscountPrice * (obj.discountPrice - obj.fullPrice));
+//             },
+//             carrierBagCharge: function (obj) {
+//                 return obj.numberOfBags * obj.chargePerBag;
+//             }
+//         };
+//     }());
+//
+//     module.exports = rules;
+//
+// }());
