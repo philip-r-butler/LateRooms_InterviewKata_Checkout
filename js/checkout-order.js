c6284a8c00bb515c3cc396b4132b92d280b45446
philip-r-butler <philip.r.butler@gmail.com>
2017-07-17 22:16:34 +0100

    Refactored for loops. Removed commented-out code.

--- js/checkout-order.js
+++ js/checkout-order.js
@@ -14,7 +14,6 @@
     var order;
 
     order = (function () {
-        var orderItems;
         var sku;
         var units;
         var rules;
@@ -26,7 +25,6 @@
         var getDiscountLimit;
         var getDiscountPrice;
         var getFullPrice;
-        var countItems;
         var calculateUnitCharge;
         var calculateOrderCharge;
 
@@ -93,14 +91,13 @@
         };
 
         calculateOrderCharge = function () {
-            var key;
             var charge;
 
             charge = 0;
 
-            for (key in units) {
+            Object.keys(units).forEach(function (key) {
                 charge += calculateUnitCharge(sku[key], units[key]);
-            }
+            });
 
             return charge;
         };
@@ -130,13 +127,13 @@
             },
             count: function () {
                 var num;
-                var key;
 
                 num = 0;
 
-                for (key in units) {
+                Object.keys(units).forEach(function (key) {
                     num += units[key];
-                }
+                });
+
                 return num;
             },
             countItem: function (key) {
@@ -167,145 +164,3 @@
     }
 
 }());
-
-
-// (function () {
-//
-//     "use strict";
-//     var order;
-//     var stockKeepingUnits;
-//     var rules;
-//     var billing;
-//
-//     stockKeepingUnits = require("./checkout-stockkeepingunits");
-//     rules = require("./checkout-chargerules");
-//     billing = require("./checkout-billing");
-//
-//     order = (function (sku, rules) {
-//         var orderItems;
-//         var hasChargeRule;
-//         var hasDiscount;
-//         var hasDiscountLimit;
-//         var hasDiscountPrice;
-//         var getDiscountLimit;
-//         var getDiscountPrice;
-//         var getFullPrice;
-//         var calculateCharge;
-//
-//         orderItems = [];
-//
-//         hasChargeRule = function (unit) {
-//             return unit.hasOwnProperty("rule");
-//         };
-//
-//         hasDiscount = function (unit) {
-//             return unit.hasOwnProperty("discount");
-//         };
-//
-//         hasDiscountLimit = function (unit) {
-//             return hasDiscount(unit) && unit.discount.hasOwnProperty("limit");
-//         };
-//
-//         hasDiscountPrice = function (unit) {
-//             return hasDiscount(unit) && unit.discount.hasOwnProperty("price");
-//         };
-//
-//         getDiscountLimit = function (unit) {
-//             if (hasDiscountLimit(unit)) {
-//                 return unit.discount.limit;
-//             } else {
-//                 return null;
-//             }
-//         };
-//
-//         getDiscountPrice = function (unit) {
-//             if (hasDiscountPrice(unit)) {
-//                 return unit.discount.price();
-//             } else {
-//                 return null;
-//             }
-//         };
-//
-//         getFullPrice = function (unit) {
-//             return unit.price;
-//         };
-//
-//         calculateCharge = function (unit, num) {
-//             var params;
-//
-//             params = {};
-//
-//             if (hasChargeRule(unit)) {
-//                 params = unit.rule.params;
-//                 params.count = num;
-//                 return unit.rule.func(params);
-//             } else {
-//                 params.count = num;
-//                 params.fullPrice = getFullPrice(unit);
-//                 params.discountPrice = getDiscountPrice(unit);
-//                 params.discountLimit = getDiscountLimit(unit);
-//                 return rules.discountPriceWithOrderLimit(params);
-//             }
-//         };
-//
-//         return {
-//             add: function (key) {
-//                 orderItems.push(key);
-//                 billing.update();
-//                 return this;
-//             },
-//             remove: function () {
-//                 orderItems.pop();
-//                 billing.update();
-//                 return this;
-//             },
-//             get: function () {
-//                 return orderItems;
-//             },
-//             getItem: function (index) {
-//                 return orderItems[index];
-//             },
-//             count: function () {
-//                 return orderItems.length;
-//             },
-//             countItem: function (key) {
-//                 var result;
-//                 result = 0;
-//                 // Loop through order and count items that match item
-//                 orderItems.forEach(function (i) {
-//                     if (i === key) {
-//                         result += 1;
-//                     }
-//                 });
-//                 return result;
-//             },
-//             charge: function () {
-//                 var key;
-//                 var units;
-//                 var unit;
-//                 var unitCount;
-//                 var charge;
-//
-//                 charge = 0;
-//                 units = sku.get();
-//
-//                 for (key in units) {
-//                     if (units.hasOwnProperty(key)) {
-//                         unit = units[key];
-//                         unitCount = this.countItem(key);
-//                         charge += calculateCharge(unit, unitCount);
-//                     }
-//                 }
-//                 return charge;
-//             },
-//             clear: function () {
-//                 orderItems = [];
-//                 billing.update();
-//                 return this;
-//             }
-//         };
-//     }(stockKeepingUnits, rules));
-//
-//     module.exports = order;
-//
-// }());
