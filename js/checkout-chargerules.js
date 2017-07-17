5f35fca482175687edd61ab666b7e0fb63b72d0f
philip-r-butler <philip.r.butler@gmail.com>
2017-07-17 22:15:22 +0100

    Removed commented-out code.

--- js/checkout-chargerules.js
+++ js/checkout-chargerules.js
@@ -38,37 +38,3 @@
     }
 
 }());
-
-// (function () {
-//     "use strict";
-//
-//     var rules;
-//
-//     rules = (function () {
-//
-//         return {
-//             fixedPrice: function (obj) {
-//                 return obj.price * obj.count;
-//             },
-//             discountPriceWithOrderLimit: function (obj) {
-//                 var countAtDiscountPrice;
-//
-//                 countAtDiscountPrice = (function (count, limit) {
-//                     if (limit && count) {
-//                         return Math.floor(count / limit) * limit;
-//                     } else {
-//                         return null;
-//                     }
-//                 }(obj.count, obj.discountLimit));
-//
-//                 return (obj.count * obj.fullPrice) + (countAtDiscountPrice * (obj.discountPrice - obj.fullPrice));
-//             },
-//             carrierBagCharge: function (obj) {
-//                 return obj.numberOfBags * obj.chargePerBag;
-//             }
-//         };
-//     }());
-//
-//     module.exports = rules;
-//
-// }());
